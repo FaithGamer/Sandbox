975dc2e308cbc0e1d08160c6754bfe0a223094bf
faith <darkcrecerelle@gmail.com>
2022-09-10 19:17:17 +0200

    SDL window

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -3,19 +3,39 @@
 #include <any>
 #include <stdio.h>
 
-#include "SDL.h"
+#include <SDL.h>
+#
 
 int main(int argc, char** argv)
 {
 
-	float monFloat = 14;
+	SDL_Window* window = nullptr;
+	SDL_Surface* surface = nullptr;
 
-	std::any Anything = monFloat;
-	
-	if (Anything.type() == std::make_any<float>().type())
+	int init_error = SDL_Init(SDL_INIT_VIDEO);
+	if (init_error < 0)
 	{
-		std::cout << "oui" << std::endl;
+		std::cerr << "SDL_init error " << SDL_GetError() << std::endl;
+		return 1;
 	}
+
+	window = SDL_CreateWindow(std::string("Application").c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 620, 480, SDL_WINDOW_SHOWN);
+
+	if (!window)
+	{
+		std::cerr << "SDL_Window error " << SDL_GetError() << std::endl;
+	}
+
+	surface = SDL_GetWindowSurface(window);
+
+	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 60, 6, 6));
+
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Delay(2000);
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return 0;
 }
